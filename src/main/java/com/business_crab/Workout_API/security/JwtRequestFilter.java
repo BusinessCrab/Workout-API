95fff19ef2ea7d5ae44003ddaa06f13366af5f54
Nikita Liubimtsev <zixxxota@gmail.com>
2025-11-03 21:37:45 +0300

    added jwt filter

--- src/main/java/com/business_crab/Workout_API/security/JwtRequestFilter.java
+++ src/main/java/com/business_crab/Workout_API/security/JwtRequestFilter.java
@@ -1,7 +1,53 @@
 package com.business_crab.Workout_API.security;
 
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import io.jsonwebtoken.io.IOException;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 @Component
-public class JwtRequestFilter {
+public class JwtRequestFilter extends OncePerRequestFilter{
+    private final UserDetailsService userDetailsService;
+    private final JwtUtil jwtUtil;
+
+    public JwtRequestFilter(final UserDetailsService userDetailsService , final JwtUtil jwtUtil) {
+        this.userDetailsService = userDetailsService;
+        this.jwtUtil = jwtUtil;
+    }
+
+    @Override
+    protected void doFilterInternal(final HttpServletRequest request ,
+                                    final HttpServletResponse response , 
+                                    final FilterChain chain)
+    throws ServletException , IOException {
+        final String authorizationHeader = request.getHeader("Authorization");
+        String username = null , jwt = null;
+        if (authorizationHeader != null && authorizationHeader.startsWith("Bearer ")) {
+            jwt = authorizationHeader.substring(7);
+            username = jwtUtil.extractUsername(jwt);
+        }
+
+        if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+            final UserDetails userDetails = this.userDetailsService.loadUserByUsername(username);
+            if (jwtUtil.validateToken(jwt, userDetails)) {
+                UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(userDetails , null , userDetails.getAuthorities());
+                authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                SecurityContextHolder.getContext().setAuthentication(authToken);
+            }
+        }
+        try {
+            chain.doFilter(request , response);
+        } catch(final Exception exception) {
+            System.err.println(exception.getMessage());
+        }
+    }
 }
