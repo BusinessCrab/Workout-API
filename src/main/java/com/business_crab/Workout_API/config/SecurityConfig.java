5b8e1081cdbe59e3dafb8f36c3e92b356333221d
Nikita Liubimtsev <zixxxota@gmail.com>
2025-11-03 21:51:40 +0300

    configured security file

--- src/main/java/com/business_crab/Workout_API/config/SecurityConfig.java
+++ src/main/java/com/business_crab/Workout_API/config/SecurityConfig.java
@@ -1,5 +1,51 @@
 package com.business_crab.Workout_API.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import com.business_crab.Workout_API.security.JwtRequestFilter;
+
+@Configuration
+@EnableWebSecurity
 public class SecurityConfig {
-    
+    private final JwtRequestFilter jwtRequestFilter;
+
+    public SecurityConfig(final JwtRequestFilter jwtRequestFilter) {
+        this.jwtRequestFilter = jwtRequestFilter;
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(final HttpSecurity http) throws Exception {
+        http.csrf(AbstractHttpConfigurer::disable)
+            .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+            .authorizeHttpRequests(auth -> auth.requestMatchers("/swagger-ui/**","/swagger-ui/index.html", "/api-docs/**").permitAll()
+                                               .requestMatchers("/api/auth/signup", "/api/auth/login").permitAll()
+                                               .requestMatchers("/api/users/**").hasRole("ADMIN")
+                                               .requestMatchers("/api/exercises/**").authenticated()
+                                               .requestMatchers("/api/workout-plans/**", "/api/workout-schedules/**", "/api/reports/**").authenticated()
+                                               .anyRequest().authenticated()
+                                  )
+            .addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
+        return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(final AuthenticationConfiguration authConfig) throws Exception {
+        return authConfig.getAuthenticationManager();
+    }
 }
